7df8a5796c950dd44690f6344172fab4199d3d9f
Yonadav Shavit <yonadav@mit.edu>
2016-11-19 13:50:25 -0500

    Changed game creation signature

--- yo-playground.py
+++ yo-playground.py
@@ -9,36 +9,36 @@ NUM_VALIDATION_POINTS = 10
 
 model = Sequential()
 
-model.add(Dense(output_dim=5000,input_dim=3))
+model.add(Dense(output_dim=500,input_dim=3))
 model.add(Activation("relu"))
-model.add(Dense(output_dim=5000))
-model.add(Activation("relu"))
-#model.add(Dense(output_dim=2))
-model.add(Dense(output_dim=1))
+# model.add(Dense(output_dim=500))
+# model.add(Activation("relu"))
+model.add(Dense(output_dim=2))
+#model.add(Dense(output_dim=1))
 model.add(Activation("linear"))
-model.compile(optimizer='rmsprop',loss="mse",metrics=["mean_squared_error"])
+model.compile(optimizer='adagrad',loss="mse",metrics=["mean_squared_error"])
 
 data = np.random.random((NUM_EXAMPLES,3))
-f_a = lambda x,y,z: np.multiply(x,y) + 3*z
-f_b = lambda x,y,z: np.multiply(x,y) - 3*z
+f_a = lambda x,y,z: x + 2*y + 3*z#np.multiply(x,y) + 3*z
+f_b = lambda x,y,z: x + 2*y - 3*z#np.multiply(x,y) - 3*z
 labels_a = f_a(data[:,0],data[:,1],data[:,2])
 labels_b = f_b(data[:,0],data[:,1],data[:,2])
-#labels = np.asarray([labels_a,labels_b]).transpose()
-labels = labels_a
+labels = np.asarray([labels_a,labels_b]).transpose()
+#labels = labels_a
 
-model.fit(data,labels,nb_epoch=10,batch_size=32)
+model.fit(data,labels,nb_epoch=1000,batch_size=32)
 
 validation_data = np.random.random((NUM_VALIDATION_POINTS,3))
 validation_labels_a = f_a(data[:,0],data[:,1],data[:,2])
 validation_labels_b = f_b(data[:,0],data[:,1],data[:,2])
 predicted_labels = model.predict(validation_data,verbose=1)
 for i in xrange(NUM_VALIDATION_POINTS):
-	print "Example 1 - "
+	print "Example  - "
 	print "a: ", validation_data[i,0], \
 		", b: ", validation_data[i,1], \
 		", c: ", validation_data[i,2]
 	# print "correct result: ", validation_labels_a[i], ", ", validation_labels_b[i]
 	# print "true result: ", predicted_labels[i,0], ", ", predicted_labels[i,1]
-	print "correct result: ", validation_labels_a[i]#, ", ", validation_labels_b[i]
-	print "true result: ", predicted_labels[i]#, ", ", predicted_labels[i,1]
+	print "correct result: ", validation_labels_a[i], ", ", validation_labels_b[i]
+	print "true result: ", predicted_labels[i]
 
