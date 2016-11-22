0d28154c7b018f545675b84f1dcb9beaa1644f0b
Rotem Hemo <rotemhemo@dhcp-18-189-84-134.dyn.mit.edu>
2016-11-22 13:56:45 -0500

    Now can specify the image name as an argument

--- learn_connect4.py
+++ learn_connect4.py
@@ -1,20 +1,21 @@
 from MCTS.sim import *
-from RL.RLAgent import *
+#from RL.RLAgent import *
 from MCTS.game import * 
 import numpy as np
 import scipy.io as sio
 import os
+import sys
+import threading
 
-def generate_supervised_training_data(num_episodes, time_limit=0.5, file_path='/media/beomjoon/My Passport/vision_project/supervised_data/train_data'):
+def generate_supervised_training_data(episode_num, time_limit=0.5, file_path=''):
   train_data= []
-  for x in xrange(num_episodes): 
-    print x
-    episode = generate_uct_game(time_limit)
-    win_player_id = np.argmax(episode[-1][-1])
-    winner_train_data = [e for e in episode if e[0] == win_player_id]
-    loser_train_data = [e for e in episode if e[0] != win_player_id]
-    sio.savemat(file_path + str(x)+'.mat',{'winner_train_data':winner_train_data,'loser_train_data':loser_train_data,\
-                  'uct_time_limit':time_limit})
+  episode = generate_uct_game(time_limit)
+  win_player_id = np.argmax(episode[-1][-1])
+  winner_train_data = [e for e in episode if e[0] == win_player_id]
+  loser_train_data = [e for e in episode if e[0] != win_player_id]
+  sio.savemat(file_path + str(episode_num)+'.mat',{'winner_train_data':winner_train_data,'loser_train_data':loser_train_data,\
+                'uct_time_limit':time_limit})
+  return
 
 def load_supervised_training_data( train_dir ):
   train_files = os.listdir(train_dir)
@@ -36,12 +37,18 @@ def load_supervised_training_data( train_dir ):
   return np.asarray(s_data),np.asarray(a)
 
 def main():
-  # generate_supervised_training_data(100000)
+  if len(sys.argv) < 2:
+    raise Exception("Syntax: python %s episode_numbers" % (sys.argv[0]))
+  else:
+    num_of_episodes = int(sys.argv[1])
+
+  generate_supervised_training_data(num_of_episodes)
+
   # obtain training data
-  s_data,a = load_supervised_training_data('./train_data')
-  rl_agent = ReinforcementLearningAgent(144,8)
-  rl_agent.update_supervised_policy(s_data,a)
-  episode = generate_custom_policy_game(rl_agent,rl_agent)
+  # s_data,a = load_supervised_training_data('./train_data')
+  # rl_agent = ReinforcementLearningAgent(144,8)
+  # rl_agent.update_supervised_policy(s_data,a)
+  # episode = generate_custom_policy_game(rl_agent,rl_agent)
 
 if __name__ == '__main__':
     main()
