fe35d500a953a90399386948e40b465354bf5fff
BEOMJOON <beomjoon@mit.edu>
2016-11-29 13:01:19 -0500

    getting rid of break point

--- learn_sup_v_network.py
+++ learn_sup_v_network.py
@@ -10,8 +10,6 @@ from tester import test_policy_vs_MCTS
 def main():
   train_dataset_dir = '../dataset/'
   s_data,_,_,player_id,_,value= load_supervised_training_data(train_dataset_dir)
-  import pdb;pdb.set_trace()
-  
   rl_agent = SupervisedValueNetworkAgent((144,144,3),8)
   rl_agent.update_v_network(s_data,player_id,value)
 
