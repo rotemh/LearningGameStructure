9a9ecde4f11f95ea41c57d5faccd1a5854d5514b
Yonadav Shavit <yonadav@mit.edu>
2016-12-02 14:12:13 -0500

    Added basic AMCTS algorithm

--- MCTS/game.py
+++ MCTS/game.py
@@ -496,7 +496,7 @@ class Node(object):
     A class that represents nodes in the MCTS tree.
     """
 
-    def __init__(self, board, action=None, parent=None):
+    def __init__(self, board, action=None, parent=None, heuristic=None):
         """
         Create new node.
 
@@ -511,7 +511,11 @@ class Node(object):
         self.parent = parent
         self.children = [] # children nodes
         self.num_visits = 0 # number of times node has been visited
-        self.q = 0.0 # simulation reward
+        if heuristic == None:
+            self.q = 0.0
+        else:
+            self.q = heuristic(board)
+        self.heuristic = heuristic
         
     def get_action(self):
         """
@@ -533,6 +537,13 @@ class Node(object):
         """
 
         return self.parent
+
+    def get_heuristic(self):
+        """
+        Return node's initial heuristic.
+        """
+
+        return self.heuristic
 
     def get_children(self):
         """
---
+++ test_amcts.py
@@ -0,0 +1,39 @@
+from MCTS.sim import *
+from RL.SupervisedPolicy import *
+from RL.SupervisedValueNetwork import *
+from MCTS.game import * 
+import numpy as np
+import scipy.io as sio
+import os
+from tester import test_policy_vs_MCTS
+import tensorflow as tf
+from keras import backend as K
+from learn_sup_policy_network import load_supervised_training_data
+
+
+def amcts(policy_agent, value_agent, time_limit):
+
+  value_player = RLPlayer('value_player', value_agent)
+  policy_player = RLPlayer('policy_player', policy_agent)
+
+  def uct_heuristic(board):
+    return value_player.choose_action(board)
+
+  def default_heuristic(board):
+    return policy_player.choose_action(board)
+
+  algo = uct_with_heuristics(time_limit, uct_heuristic, default_heuristic)
+  return algo
+
+def main():
+  policy_agent = SupervisedPolicyAgent((144,144,3),8)
+  policy_agent.load_train_results()
+  value_agent = SupervisedValueNetworkAgent((144,144,3),8)
+  value_agent.load_train_results()
+  amcts_algo = amcts(policy_agent,value_agent,1)
+  acmts_player = game.RLPlayer('amcts', amcts_algo)
+  
+  test_policy_vs_MCTS(amcts,mcts_times=[0.5],verbose=True)
+
+if __name__ == '__main__':
+    main()
