2433ed5778316c5de015bd5d825a67ec466653a1
Beomjoon Kim <beomjoon@phaedra.csail.mit.edu>
2016-12-08 13:15:46 -0500

    hardcoding paths for dataset

--- learn_sup_v_network.py
+++ learn_sup_v_network.py
@@ -8,7 +8,7 @@ from learn_sup_policy_network import load_supervised_training_data
 from tester import test_policy_vs_MCTS
 
 def main():
-  train_dataset_dir = '/home/aradhana/LearningGameStructure/dataset/'
+  train_dataset_dir = '/home/beomjoon/LearningGameStructure/dataset/'
   #import pdb;pdb.set_trace()
   s_data,a,player_id,r,value,_,_= load_supervised_training_data(train_dataset_dir)
   rl_agent = SupervisedValueNetworkAgent((144,144,3),7)
